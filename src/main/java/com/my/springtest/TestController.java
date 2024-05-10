292ea96d006cecdf5d91eb38b6493a13aea10627
m.y <myakk015@gmail.com>
2024-05-11 00:34:24 +0400

    1

---
+++ src/main/java/com/my/springtest/TestController.java
@@ -0,0 +1,13 @@
+package com.my.springtest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/hi")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
