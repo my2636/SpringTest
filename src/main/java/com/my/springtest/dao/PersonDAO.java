5636194a8bf395fbaf1a3dec741dc8b982cc29d9
m.y <myakk015@gmail.com>
2024-05-15 02:41:22 +0400

    DAO, model

---
+++ src/main/java/com/my/springtest/dao/PersonDAO.java
@@ -0,0 +1,28 @@
+package com.my.springtest.dao;
+
+import com.my.springtest.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
